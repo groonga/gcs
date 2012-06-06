b966e68056a85c8481b19e23ab80ee5e4a094aa3
Yoji SHIDARA <dara@shidara.net>
2012-06-06 17:01:18 +0900

    Humanize js that is generated by coffee

--- test/croonga.test.js
+++ test/croonga.test.js
@@ -1,38 +1,31 @@
-(function() {
-  var http, run, should, spawn;
+var http = require('http');
+var should = require('should');
+var spawn = require('child_process').spawn;
 
-  should = require('should');
-
-  spawn = require('child_process').spawn;
-
-  http = require('http');
-
-  run = function(options, callback) {
-    var command, commandPath, output;
-    commandPath = __dirname + '/../bin/croonga';
-    command = spawn(commandPath, options);
-    output = {
-      stdout: '',
-      stderr: ''
-    };
-    command.stdout.on('data', function(data) {
-      return output.stdout += data;
-    });
-    command.stderr.on('data', function(data) {
-      return output.stderr += data;
-    });
-    return callback(null, command, output);
+function run(options, callback) {
+  var command, commandPath, output;
+  commandPath = __dirname + '/../bin/croonga';
+  command = spawn(commandPath, options);
+  output = {
+    stdout: '',
+    stderr: ''
   };
+  command.stdout.on('data', function(data) {
+    output.stdout += data;
+  });
+  command.stderr.on('data', function(data) {
+    output.stderr += data;
+  });
+  callback(null, command, output);
+}
 
-  describe('croonga command', function() {
-    return it('should output help for --help', function(done) {
-      return run(['--help'], function(error, command, output) {
-        return command.on('exit', function() {
-          output.stdout.should.include("Usage:");
-          return done();
-        });
+describe('croonga command', function() {
+  it('should output help for --help', function(done) {
+    run(['--help'], function(error, command, output) {
+      command.on('exit', function() {
+        output.stdout.should.include("Usage:");
+        done();
       });
     });
   });
-
-}).call(this);
+});
