52e985c10008d5e1795a7c71648d6f6b239894fa
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-06-07 19:56:14 +0900

    Add tests for Translator#translate and Translator#translateOne

--- test/batch-translator.test.js
+++ test/batch-translator.test.js
@@ -20,18 +20,8 @@ suite('batch translator', function() {
     assert.equal(translator.table, 'test');
   });
 
-  test('add', function() {
-    var batch = {
-          'type': 'add',
-          'id': 'id'+parseInt(Math.random() * 1000),
-          'version': parseInt(Math.random() * 1000),
-          'lang': 'en',
-          'fields': {
-            'name': 'Meat Guy',
-            'birhday': '2929-02-09',
-            'job': 'Meat Guy'
-          }
-        };
+  test('addToLoad', function() {
+    var batch = BATCH_ADD_MEAT_GUY;
     var expected = 'load --table test ' + JSON.stringify([{
           '_key': batch['id'],
           'name': batch['fields']['name'],
@@ -41,4 +31,44 @@ suite('batch translator', function() {
     var translated = translator.addToLoad(batch);
     assert.equal(translated, expected);
   });
+
+  test('translateOne for add', function() {
+    var batch = BATCH_ADD_MEAT_GUY;
+    var expected = 'load --table test ' + JSON.stringify([{
+          '_key': batch['id'],
+          'name': batch['fields']['name'],
+          'birhday': batch['fields']['birhday'],
+          'job': batch['fields']['job']
+        }]);
+    var translated = translator.translateOne(batch);
+    assert.equal(translated, expected);
+  });
+
+  test('translate', function() {
+    var batches = BATCHES;
+    var batch = batches[0];
+    var expected = 'load --table test ' + JSON.stringify([{
+          '_key': batch['id'],
+          'name': batch['fields']['name'],
+          'birhday': batch['fields']['birhday'],
+          'job': batch['fields']['job']
+        }]);
+    var translated = translator.translate(BATCHES);
+    assert.equal(translated, expected);
+  });
 });
+
+var BATCH_ADD_MEAT_GUY = {
+      'type': 'add',
+      'id': 'id'+parseInt(Math.random() * 1000),
+      'version': parseInt(Math.random() * 1000),
+      'lang': 'en',
+      'fields': {
+        'name': 'Meat Guy',
+        'birhday': '2929-02-09',
+        'job': 'Meat Guy'
+      }
+    };
+var BATCHES = [
+      BATCH_ADD_MEAT_GUY
+    ];
