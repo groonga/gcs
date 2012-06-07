57c7542f254caab36b04305fc497b6558971acf8
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-06-07 19:00:00 +0900

    Add rmTree and rmTreeSync for tests

--- test/batch.test.js
+++ test/batch.test.js
@@ -1,12 +1,23 @@
+var utils = require('test-utils');
+
+
 var assert = require('chai').assert;
+var fs = require('fs');
+var path = require('path');
+
+var BatchProcessor = require('../lib/batch').BatchProcessor;
+
+suiteSetup(function() {
+  utils.recreateTemporaryDirectory();
+});
 
 suite('batch', function() {
-  setup(function() {
-  });
-
-  test('test', function() {
-    var actual = 'foo';
-    var expected = 'foo';
-    assert.equal(actual, expected);
+  test('initialize', function() {
+    var processor = new BatchProcessor({
+          databasePath: utils.databasePath,
+          domain: 'test-domain'
+        });
+    assert.equal(processor.databasePath, databasePath);
+    assert.equal(processor.domain, 'test-domain');
   });
 });
