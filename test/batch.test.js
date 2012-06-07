8082d9acc6f1c219de946ec156bb6343c657d572
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-06-07 18:15:33 +0900

    Add test for batch based on chai

---
+++ test/batch.test.js
@@ -0,0 +1,12 @@
+var assert = require('chai').assert;
+
+suite('batch', function() {
+  setup(function() {
+  });
+
+  test('test', function() {
+    var actual = 'foo';
+    var expected = 'foo';
+    assert.equal(actual, expected);
+  });
+});
