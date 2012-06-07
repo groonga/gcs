9134ca10fa1111cb0f7bde2dc4917baca76b800c
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-06-07 19:46:45 +0900

    Add test for Translator#addToLoad

--- test/batch-translator.test.js
+++ test/batch-translator.test.js
@@ -5,9 +5,40 @@ var assert = require('chai').assert;
 var Translator = require('../lib/batch/translator').Translator;
 
 suite('batch translator', function() {
+  var translator;
+
+  setup(function() {
+    translator = new Translator('test');
+  });
+
+  teardown(function() {
+    translator = undefined;
+  });
+
   test('initialize', function() {
-    var translator = new Translator('test');
     assert.equal(translator.domain, 'test');
     assert.equal(translator.table, 'test');
+  });
+
+  test('add', function() {
+    var batch = {
+          'type': 'add',
+          'id': 'id'+parseInt(Math.random() * 1000),
+          'version': parseInt(Math.random() * 1000),
+          'lang': 'en',
+          'fields': {
+            'name': 'Meat Guy',
+            'birhday': '2929-02-09',
+            'job': 'Meat Guy'
+          }
+        };
+    var expected = 'load --table test ' + JSON.stringify([
+          '_key': input['id'],
+          'name': input['fields']['name'],
+          'birhday': input['fields']['birhday'],
+          'job': input['fields']['job']
+        ]);
+    var translated = translator.addToLoad(batch);
+    assert.equal(translated, expected);
   });
 });
