88e98ab037827098592f187f7ef1d0dc766b54c4
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-06-11 16:20:08 +0900

    Translate batches to commands and options for nroonga (instead of .grn dumps)

--- test/batch-translator.test.js
+++ test/batch-translator.test.js
@@ -4,7 +4,7 @@ var assert = require('chai').assert;
 
 var Translator = require('../lib/batch/translator').Translator;
 
-suite('batch translator', function() {
+suite('batch/translator/Translator (instance methods)', function() {
   var translator;
 
   setup(function() {
@@ -22,39 +22,130 @@ suite('batch translator', function() {
 
   test('addToLoad', function() {
     var batch = BATCH_ADD_MEAT_GUY;
-    var expected = 'load --table test ' + JSON.stringify([{
-          '_key': batch['id'],
-          'name': batch['fields']['name'],
-          'birhday': batch['fields']['birhday'],
-          'job': batch['fields']['job']
-        }]);
+    var expected = {
+          command: 'load',
+          options: {
+            table: 'test',
+            values: JSON.stringify([{
+              '_key': batch['id'],
+              'name': batch['fields']['name'],
+              'birhday': batch['fields']['birhday'],
+              'job': batch['fields']['job']
+            }])
+          }
+        };
     var translated = translator.addToLoad(batch);
     assert.equal(translated, expected);
   });
 
   test('translateOne for add', function() {
     var batch = BATCH_ADD_MEAT_GUY;
-    var expected = 'load --table test ' + JSON.stringify([{
-          '_key': batch['id'],
-          'name': batch['fields']['name'],
-          'birhday': batch['fields']['birhday'],
-          'job': batch['fields']['job']
-        }]);
+    var expected = {
+          command: 'load',
+          options: {
+            table: 'test',
+            values: JSON.stringify([{
+              '_key': batch['id'],
+              'name': batch['fields']['name'],
+              'birhday': batch['fields']['birhday'],
+              'job': batch['fields']['job']
+            }])
+          }
+        };
     var translated = translator.translateOne(batch);
     assert.equal(translated, expected);
   });
 
   test('translate', function() {
     var batches = BATCHES;
-    var batch = batches[0];
-    var expected = 'load --table test ' + JSON.stringify([{
-          '_key': batch['id'],
-          'name': batch['fields']['name'],
-          'birhday': batch['fields']['birhday'],
-          'job': batch['fields']['job']
-        }]);
+    var expected = [
+          {
+            command: 'load',
+            options: {
+              table: 'test',
+              values: JSON.stringify([{
+                '_key': batches[0]['id'],
+                'name': batches[0]['fields']['name'],
+                'birhday': batches[0]['fields']['birhday'],
+                'job': batches[0]['fields']['job']
+              }])
+            }
+          },
+          {
+            command: 'load',
+            options: {
+              table: 'test',
+              values: JSON.stringify([{
+                '_key': batches[1]['id'],
+                'name': batches[1]['fields']['name'],
+                'birhday': batches[1]['fields']['birhday'],
+                'job': batches[1]['fields']['job']
+              }])
+            }
+          }
+        ];
     var translated = translator.translate(BATCHES);
     assert.equal(translated, expected);
+  });
+});
+
+suite('batch/translator/Translator (class methods)', function() {
+  suite('commandToString', function() {
+    test('load ', function() {
+      var batch = BATCH_ADD_MEAT_GUY;
+      var command = {
+            command: 'load',
+            options: {
+              table: 'test',
+              values: JSON.stringify([{
+                '_key': batch['id'],
+                'name': batch['fields']['name'],
+                'birhday': batch['fields']['birhday'],
+                'job': batch['fields']['job']
+              }])
+            }
+          };
+      var expected = 'load --table test --values '+command.options.values;
+      var stringified = Translator.commandToString(command);
+      assert.equal(stringified, expected);
+    });
+  });
+
+  suite('commandsToString', function() {
+    test('load ', function() {
+      var commands = [
+            {
+              command: 'load',
+              options: {
+                table: 'test',
+                values: JSON.stringify([{
+                  '_key': batches[0]['id'],
+                  'name': batches[0]['fields']['name'],
+                  'birhday': batches[0]['fields']['birhday'],
+                  'job': batches[0]['fields']['job']
+                }])
+              }
+            },
+            {
+              command: 'load',
+              options: {
+                table: 'test',
+                values: JSON.stringify([{
+                  '_key': batches[1]['id'],
+                  'name': batches[1]['fields']['name'],
+                  'birhday': batches[1]['fields']['birhday'],
+                  'job': batches[1]['fields']['job']
+                }])
+              }
+            }
+          ];
+      var expected = [
+            'load --table test --values ' + commands[0].options.values,
+            'load --table test --values ' + commands[1].options.values
+          ].join('\n');
+      var stringified = Translator.commandsToString(commands);
+      assert.equal(stringified, expected);
+    });
   });
 });
 
@@ -69,6 +160,18 @@ var BATCH_ADD_MEAT_GUY = {
         'job': 'Meat Guy'
       }
     };
+var BATCH_ADD_MEAT_LADY = {
+      'type': 'add',
+      'id': 'id2929',
+      'version': 2929,
+      'lang': 'en',
+      'fields': {
+        'name': 'Meat Lady',
+        'birhday': '2929-02-09',
+        'job': 'Meat Lady'
+      }
+    };
 var BATCHES = [
-      BATCH_ADD_MEAT_GUY
+      BATCH_ADD_MEAT_GUY,
+      BATCH_ADD_MEAT_LADY
     ];
