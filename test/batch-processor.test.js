9bba067c18fd79bfe490c08025b3718591c47012
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-06-11 18:30:17 +0900

    Add assertion for the result of "dump" command after loading

--- test/batch-processor.test.js
+++ test/batch-processor.test.js
@@ -53,6 +53,10 @@ suite('batch/processor/Processor (instance methods)', function() {
     processor.process(BATCHES)
       .next(function(results) {
         assert.deepEqual(results, [1, 1]);
+        var dump = database.commandSync('dump', {
+              table: 'test'
+            });
+        assert.equal(dump, DUMP);
         done();
       })
       .error(function(error) {
@@ -87,3 +91,13 @@ var BATCHES = [
       BATCH_ADD_MEAT_GUY,
       BATCH_ADD_MEAT_LADY
     ];
+var DUMP = 'table_create test TABLE_HASH_KEY ShortText\n' +
+           'column_create test name COLUMN_SCALAR ShortText\n' +
+           'column_create test birthday COLUMN_SCALAR ShortText\n' +
+           'column_create test job COLUMN_SCALAR ShortText\n' +
+           'load --table test\n' +
+           '[\n' +
+           '["_key","name","birthday","job"],\n' +
+           '["id29","Meat Guy","2929-02-09","Meat Guy"],\n' +
+           '["id2929","Meat Lady","2929-02-09","Meat Lady"]\n' +
+           ']';
