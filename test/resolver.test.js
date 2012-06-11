bfb782f8769fbbb7ff3bb8d8e4a4af460f6d836a
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-06-11 15:04:52 +0900

    Validate field names for translations

--- test/resolver.test.js
+++ test/resolver.test.js
@@ -47,4 +47,48 @@ suite('resolver', function() {
       }, /"D", "N" cannot appear in a domain name/)
     });
   });
+
+  suite('getColumnNameFromField', function() {
+    test('lower case', function() {
+      var columnName = resolver.getColumnNameFromField('valid');
+      assert.equal(columnName, 'valid');
+    });
+
+    test('lower case and number', function() {
+      var columnName = resolver.getColumnNameFromField('valid123');
+      assert.equal(columnName, 'valid123');
+    });
+
+    test('lower case, number amd underscore', function() {
+      var columnName = resolver.getColumnNameFromField('valid_123');
+      assert.equal(columnName, 'valid_123');
+    });
+
+    test('too short', function() {
+      assert.throw(function() {
+        resolver.getColumnNameFromField('va');
+      }, /too short field name/)
+    });
+
+    test('too long', function() {
+      assert.throw(function() {
+        resolver.getColumnNameFromField('abcdefghijklmnopqrstuvwxyz' +
+                                        '0123456789' +
+                                        'abcdefghijklmnopqrstuvwxyz' +
+                                        '0123456789');
+      }, /too long field name/)
+    });
+
+    test('hyphen', function() {
+      assert.throw(function() {
+        resolver.getColumnNameFromField('field-name');
+      }, /"-" cannot appear in a field name/)
+    });
+
+    test('upper case', function() {
+      assert.throw(function() {
+        resolver.getColumnNameFromField('FieldName');
+      }, /"F", "N" cannot appear in a field name/)
+    });
+  });
 });
