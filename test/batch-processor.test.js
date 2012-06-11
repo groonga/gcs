e7f8728789ea169183720d1ed4e1f7c75c9fd9b8
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-06-11 16:42:32 +0900

    Implement Processor#process

--- test/batch-processor.test.js
+++ test/batch-processor.test.js
@@ -10,13 +10,61 @@ suiteSetup(function() {
   utils.prepareCleanTemporaryDatabase();
 });
 
-suite('batch processor', function() {
+suite('batch/processor/Processor (instance methods)', function() {
+  var processor;
+
+  setup(function() {
+    processor = new Processor({
+      databasePath: utils.databasePath,
+      domain: 'test'
+    });
+  });
+
+  teardown(function() {
+    processor = undefined;
+  });
+
   test('initialize', function() {
-    var processor = new Processor({
-          databasePath: utils.databasePath,
-          domain: 'test-domain'
-        });
     assert.equal(processor.databasePath, utils.databasePath);
-    assert.equal(processor.domain, 'test-domain');
+    assert.equal(processor.domain, 'test');
+  });
+
+  test('process load', function(done) {
+    processor.process(BATCHES)
+      .next(function(results) {
+        assert.deepEqual(results, [1, 1]);
+        done();
+      })
+      .error(function(error) {
+        assertNull(error); // to handle error in the method
+        done();
+      });
   });
 });
+
+var BATCH_ADD_MEAT_GUY = {
+      'type': 'add',
+      'id': 'id29',
+      'version': 29,
+      'lang': 'en',
+      'fields': {
+        'name': 'Meat Guy',
+        'birhday': '2929-02-09',
+        'job': 'Meat Guy'
+      }
+    };
+var BATCH_ADD_MEAT_LADY = {
+      'type': 'add',
+      'id': 'id2929',
+      'version': 2929,
+      'lang': 'en',
+      'fields': {
+        'name': 'Meat Lady',
+        'birhday': '2929-02-09',
+        'job': 'Meat Lady'
+      }
+    };
+var BATCHES = [
+      BATCH_ADD_MEAT_GUY,
+      BATCH_ADD_MEAT_LADY
+    ];
