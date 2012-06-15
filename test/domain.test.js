d9c75b4570f02e6c600983384673615161d13ee5
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-06-15 17:07:18 +0900

    Re-implement methods to get colum names as the new class IndexField

--- test/domain.test.js
+++ test/domain.test.js
@@ -2,10 +2,12 @@ var utils = require('./test-utils');
 
 var assert = require('chai').assert;
 
-var Domain = require('../lib/domain').Domain;
+var domain = require('../lib/domain');
+var Domain = domain.Domain;
+var IndexField = domain.IndexField;
 
-suite('Domain', function() {
-  suite('table', function() {
+suite('domain', function() {
+  suite('Domain', function() {
     test('lower case', function() {
       var domain = new Domain('valid');
       assert.equal(domain.tableName, 'valid');
@@ -45,6 +47,61 @@ suite('Domain', function() {
       assert.throw(function() {
         var domain = new Domain('DomainName');
       }, /"D", "N" cannot appear in a domain name/)
+    });
+  });
+
+  suite('IndexField', function() {
+    var domain;
+
+    setup(function() {
+      domain = new Domain('testdomain');
+    });
+
+    teardown(function() {
+      domain = undefined;
+    });
+
+    test('lower case', function() {
+      var field = new IndexField('valid', domain);
+      assert.equal(field.columnName, 'valid');
+    });
+
+    test('lower case and number', function() {
+      var field = new IndexField('valid123', domain);
+      assert.equal(field.columnName, 'valid123');
+    });
+
+    test('lower case, number amd underscore', function() {
+      var field = new IndexField('valid_123', domain);
+      assert.equal(field.columnName, 'valid_123');
+    });
+
+    test('too short', function() {
+      assert.throw(function() {
+        var field = new IndexField('va', domain);
+      }, /too short field name/)
+    });
+
+    test('too long', function() {
+      assert.throw(function() {
+        var fieldName = 'abcdefghijklmnopqrstuvwxyz' +
+                        '0123456789' +
+                        'abcdefghijklmnopqrstuvwxyz' +
+                        '0123456789';
+        var field = new IndexField(fieldName, domain);
+      }, /too long field name/)
+    });
+
+    test('hyphen', function() {
+      assert.throw(function() {
+        var field = new IndexField('field-name', domain);
+      }, /"-" cannot appear in a field name/)
+    });
+
+    test('upper case', function() {
+      assert.throw(function() {
+        var field = new IndexField('FieldName', domain);
+      }, /"F", "N" cannot appear in a field name/)
     });
   });
 });
