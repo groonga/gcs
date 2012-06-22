c52b89473b03a3fd6d240842930507e3c4bd4ef0
Yoji SHIDARA <dara@shidara.net>
2012-06-22 15:58:13 +0900

    Implement Search UI

--- public/javascripts/gcs.js
+++ public/javascripts/gcs.js
@@ -1,2 +1,22 @@
+function renderResults(data) {
+  var template = $('script#table-template').text();
+  var rendered = jade.compile(template)(data);
+  $(results).html(rendered);
+}
+
 $(document).ready(function($) {
+  $('form.search').submit(function() {
+    var query = $('form.search input[name="query"]').val();
+
+    $.ajax({
+      type: 'GET',
+      url: 'http://search-companies-example.localhost:3000/2011-02-01/search', // TODO
+      data: {q: query},
+      dataType: 'jsonp',
+      success: function(data) {
+        renderResults(data);
+      }
+    });
+    return false;
+  });
 });
