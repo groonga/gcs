19dc588698e90a15fbf54b56439efc0c10eb0ac1
Yoji SHIDARA <dara@shidara.net>
2012-07-05 17:36:29 +0900

    Support size parameter for search. Closes #31

--- test/search.test.js
+++ test/search.test.js
@@ -142,4 +142,52 @@ suite('Search API', function() {
       done();
     }
   );
+
+  testSearch('/2011-02-01/search?q=Tokyo&size=2',
+             'search-companies-00000000000000000000000000.localhost',
+    function(response, body, done) {
+      var actual = JSON.parse(body);
+      assert.operator(actual.info['time-ms'], '>=', 0, 'time-ms is ok');
+      actual.info['time-ms'] = 0; // always set 0 for test
+      var expected = {
+        rank: '-text_relevance',
+        'match-expr': '',
+        hits: {
+          found: 3,
+          start: 0,
+          hit: [
+            {
+              id: 'id1',
+              data: {
+                _id: [1],
+                _key: ['id1'],
+                address: ['Shibuya, Tokyo, Japan'],
+                description: [''],
+                email_address: ['info@razil.jp'],
+                name: ['Brazil']
+              }
+            },
+            {
+              id: 'id3',
+              data: {
+                _id: [3],
+                _key: ['id3'],
+                address: ['Hongo, Tokyo, Japan'],
+                description: [''],
+                email_address: ['info@clear-code.com'],
+                name: ['ClearCode Inc.']
+              }
+            }
+          ]
+        },
+        info: {
+          rid: '000000000000000000000000000000000000000000000000000000000000000',
+          'time-ms': 0, // always 0
+          'cpu-time-ms': 0
+        }
+      };
+      assert.deepEqual(actual, expected);
+      done();
+    }
+  );
 });
