7c1c5c2d9287ea860f516856c32267d0d3d62068
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-07-06 17:57:04 +0900

    Support "start" parameter for the search API

--- test/search.test.js
+++ test/search.test.js
@@ -191,4 +191,51 @@ suite('Search API', function() {
       done();
     }
   );
+
+  testSearch('/2011-02-01/search?q=Tokyo&start=1',
+             'search-companies-00000000000000000000000000.localhost',
+    function(response, body, done) {
+      var actual = JSON.parse(body);
+      actual.info['time-ms'] = 0; // always set 0 for test
+      var expected = {
+        rank: '-text_relevance',
+        'match-expr': '',
+        hits: {
+          found: 3,
+          start: 1,
+          hit: [
+            {
+              id: 'id3',
+              data: {
+                _id: [3],
+                _key: ['id3'],
+                address: ['Hongo, Tokyo, Japan'],
+                description: [''],
+                email_address: ['info@clear-code.com'],
+                name: ['ClearCode Inc.']
+              }
+            },
+            {
+              id: 'id9',
+              data: {
+                _id: [9],
+                _key: ['id9'],
+                address: ['Tokyo, Japan'],
+                description: [''],
+                email_address: [''],
+                name: ['Umbrella Corporation.']
+              }
+            }
+          ]
+        },
+        info: {
+          rid: '000000000000000000000000000000000000000000000000000000000000000',
+          'time-ms': 0, // always 0
+          'cpu-time-ms': 0
+        }
+      };
+      assert.deepEqual(actual, expected);
+      done();
+    }
+  );
 });
