ad2b5eb74fc152c74e3d283167d64858410a9166
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-07-06 15:24:15 +0900

    Implement DeleteIndexField action

--- test/api-configuration.test.js
+++ test/api-configuration.test.js
@@ -210,6 +210,54 @@ suite('Configuration API', function() {
       });
   });
 
+  test('Get, Action=DeleteIndexField', function(done) {
+    var path = '/?DomainName=companies&Action=CreateDomain&Version=2011-02-01';
+    utils.get(path, {
+                'Host': 'cloudsearch.localhost'
+              })
+      .next(function(response) {
+        var path = '/?DomainName=companies&IndexField.IndexFieldName=name&' +
+                   'Action=DefineIndexField&Version=2011-02-01';
+        return utils.get(path);
+      })
+      .next(function(response) {
+        var path = '/?DomainName=companies&IndexFieldName=name&' +
+                   'Action=DeleteIndexField&Version=2011-02-01';
+        return utils.get(path);
+      })
+      .next(function(response) {
+        var expected = {
+              statusCode: 200,
+              body: '<?xml version="1.0"?>\n' +
+                    '<DeleteIndexFieldResponse xmlns="' + XMLNS + '">' +
+                      '<DeleteIndexFieldResult/>' +
+                      '<ResponseMetadata>' +
+                        '<RequestId></RequestId>' +
+                      '</ResponseMetadata>' +
+                    '</DeleteIndexFieldResponse>'
+            };
+        var actual = {
+              statusCode: response.statusCode,
+              body: response.body
+            };
+        assert.deepEqual(actual, expected);
+
+        var dump = database.commandSync('dump', {
+              tables: 'companies'
+            });
+        var expected = 'table_create companies_BigramTerms ' +
+                         'TABLE_PAT_KEY|KEY_NORMALIZE ShortText ' +
+                         '--default_tokenizer TokenBigram\n' +
+                       'table_create companies TABLE_HASH_KEY ShortText';
+        assert.equal(dump, expected);
+
+        done();
+      })
+      .error(function(error) {
+        done(error);
+      });
+  });
+
   test('Get, no version', function(done) {
     var path = '/?Action=unknown';
     utils.get(path, {
