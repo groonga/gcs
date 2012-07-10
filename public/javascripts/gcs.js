5da889451e1e6c72bd1292765912014ae094f8e0
Yoji SHIDARA <dara@shidara.net>
2012-07-10 12:47:06 +0900

    Implement pager on web dashboard. Closes #33

--- public/javascripts/gcs.js
+++ public/javascripts/gcs.js
@@ -1,7 +1,36 @@
-function renderResults(data) {
+function renderResults(data, perPage) {
   var template = $('script#results-template').text();
   var rendered = jade.compile(template)(data);
+  var found = data.hits.found;
+  var start = data.hits.start;
+  var returned = data.hits.hit.length;
+  var nextStart = start + perPage;
+  var previousStart = data.hits.start - perPage;
   $('#results').html(rendered);
+
+  if (previousStart < 0) {
+    $('#pager .previous').addClass('disabled');
+    $('#pager .previous a').click(function() { return false; });
+  } else {
+    $('#pager .previous a').click(function() {
+      $('form#search input[name="start"]').val(previousStart);
+      searchExecute();
+    });
+  }
+
+  if (nextStart >= data.hits.found) {
+    $('#pager .next').addClass('disabled');
+    $('#pager .previous a').click(function() { return false; });
+  } else {
+    $('#pager .next a').click(function() {
+      $('form#search input[name="start"]').val(nextStart);
+      searchExecute();
+    });
+  }
+
+  var from = start + 1;
+  var to = start + returned;
+  $('#showing').text('Showing ' + from + ' to ' + to + ' of ' + found + ' Results');
 }
 
 function renderRequestInformation(data) {
@@ -10,22 +39,24 @@ function renderRequestInformation(data) {
   $('#request-information').html(rendered);
 }
 
-function onSearch() {
+function searchExecute() {
   var query = $('form#search input[name="query"]').val();
   var domain = $('form#domain input[name="domain-name"]').val();
   var searchEndpoint = 'http://search-' + domain + '-00000000000000000000000000.localhost:7575/2011-02-01/search';
-  var params = {q: query};
+  var perPage = 5;
+  var start = parseInt($('form#search input[name="start"]').val() || '0', 10);
+  var params = {q: query, size: perPage, start: start};
   var urlForRawRequest = searchEndpoint + '?' + jQuery.param(params);
   renderRequestInformation({urlForRawRequest: urlForRawRequest});
 
-  $('#results').hide();
+  $('#results').empty();
   $.ajax({
     type: 'GET',
     url: searchEndpoint,
     data: params,
     dataType: 'jsonp',
     success: function(data) {
-      renderResults(data);
+      renderResults(data, perPage);
       $('#results').show();
     }
   });
@@ -33,6 +64,6 @@ function onSearch() {
 }
 
 $(document).ready(function($) {
-  $('form#domain').submit(onSearch);
-  $('form#search').submit(onSearch);
+  $('form#domain').submit(searchExecute);
+  $('form#search').submit(searchExecute);
 });
