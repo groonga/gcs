a259f2ff7e80a07b9297cf46a3ebdfe2efbc162a
Yoji SHIDARA <dara@shidara.net>
2012-07-11 19:43:15 +0900

    Use synonyms table for search API. Refs. #35

--- test/search.test.js
+++ test/search.test.js
@@ -20,8 +20,8 @@ suite('Search API', function() {
     server.close();
   });
 
-  function testSearch(path, host, callback) {
-    test('GET ' + path, function(done) {
+  function testSearch(path, message, host, callback) {
+    test('GET ' + path + ' ' + message, function(done) {
       var options = {
         host: utils.testHost,
         port: utils.testPort,
@@ -47,6 +47,7 @@ suite('Search API', function() {
     });
 
     testSearch('/2011-02-01/search?q=Hongo',
+               'should hit one entry',
                'search-companies-00000000000000000000000000.localhost',
       function(response, body, done) {
         var actual = JSON.parse(body);
@@ -84,6 +85,7 @@ suite('Search API', function() {
     );
 
     testSearch('/2011-02-01/search?q=Tokyo',
+               'should hit three entries',
                'search-companies-00000000000000000000000000.localhost',
       function(response, body, done) {
         var actual = JSON.parse(body);
@@ -149,6 +151,7 @@ suite('Search API', function() {
     );
 
     testSearch('/2011-02-01/search?q=Tokyo&size=2',
+               'should return two hit entries',
                'search-companies-00000000000000000000000000.localhost',
       function(response, body, done) {
         var actual = JSON.parse(body);
@@ -201,6 +204,7 @@ suite('Search API', function() {
     );
 
     testSearch('/2011-02-01/search?q=Tokyo&start=1',
+               'should return offseted hit result',
                'search-companies-00000000000000000000000000.localhost',
       function(response, body, done) {
         var actual = JSON.parse(body);
@@ -212,6 +216,105 @@ suite('Search API', function() {
             found: 3,
             start: 1,
             hit: [
+              {
+                id: 'id3',
+                data: {
+                  _id: [3],
+                  _key: ['id3'],
+                  address: ['Hongo, Tokyo, Japan'],
+                  description: [''],
+                  email_address: ['info@clear-code.com'],
+                  name: ['ClearCode Inc.'],
+                  age: [3],
+                  product: ['groonga']
+                }
+              },
+              {
+                id: 'id9',
+                data: {
+                  _id: [9],
+                  _key: ['id9'],
+                  address: ['Tokyo, Japan'],
+                  description: [''],
+                  email_address: [''],
+                  name: ['Umbrella Corporation'],
+                  age: [9],
+                  product: ['tyrant']
+                }
+              }
+            ]
+          },
+          info: {
+            rid: '000000000000000000000000000000000000000000000000000000000000000',
+            'time-ms': 0, // always 0
+            'cpu-time-ms': 0
+          }
+        };
+        assert.deepEqual(actual, expected);
+        done();
+      }
+    );
+
+    testSearch('/2011-02-01/search?q=tokio',
+               'should not match with any entry',
+               'search-companies-00000000000000000000000000.localhost',
+      function(response, body, done) {
+        var actual = JSON.parse(body);
+        actual.info['time-ms'] = 0; // always set 0 for test
+        var expected = {
+          rank: '-text_relevance',
+          'match-expr': '',
+          hits: {
+            found: 0,
+            start: 0,
+            hit: [
+            ]
+          },
+          info: {
+            rid: '000000000000000000000000000000000000000000000000000000000000000',
+            'time-ms': 0, // always 0
+            'cpu-time-ms': 0
+          }
+        };
+        assert.deepEqual(actual, expected);
+        done();
+      }
+    );
+  });
+
+  suite('with fixture and synonyms loaded', function() {
+    setup(function() {
+      utils.loadDumpFile(database, __dirname + '/fixture/companies/ddl.grn');
+      utils.loadDumpFile(database, __dirname + '/fixture/companies/data.grn');
+      utils.loadDumpFile(database, __dirname + '/fixture/companies/synonyms.grn');
+    });
+
+    testSearch('/2011-02-01/search?q=tokio',
+               'should match with using synonyms',
+               'search-companies-00000000000000000000000000.localhost',
+      function(response, body, done) {
+        var actual = JSON.parse(body);
+        actual.info['time-ms'] = 0; // always set 0 for test
+        var expected = {
+          rank: '-text_relevance',
+          'match-expr': '',
+          hits: {
+            found: 3,
+            start: 0,
+            hit: [
+              {
+                id: 'id1',
+                data: {
+                  _id: [1],
+                  _key: ['id1'],
+                  address: ['Shibuya, Tokyo, Japan'],
+                  description: [''],
+                  email_address: ['info@razil.jp'],
+                  name: ['Brazil'],
+                  age: [1],
+                  product: ['groonga']
+                }
+              },
               {
                 id: 'id3',
                 data: {
