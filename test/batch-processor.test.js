3202c5e4141bb3443533db4d122d5ad91e0492d6
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-07-11 17:48:30 +0900

    Update fixtures

--- test/batch-processor.test.js
+++ test/batch-processor.test.js
@@ -48,7 +48,7 @@ suite('batch/processor/Processor (instance methods)', function() {
 
   test('getColumns', function() {
     var columns = processor.getColumns();
-    var expected = ['name', 'address', 'email_address', 'description'];
+    var expected = ['name', 'address', 'email_address', 'description', 'age'];
     assert.deepEqual(columns.sort(), expected.sort());
   });
 
