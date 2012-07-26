45125e0468aef93f7dfec805762d96c4202d9c98
Yoji SHIDARA <dara@shidara.net>
2012-07-26 16:30:50 +0900

    Add support for tinia's bq parameter
    https://github.com/dlangevin/tinia

---
+++ test/bq-translator.test.js
@@ -0,0 +1,16 @@
+var assert = require('chai').assert;
+
+var BooleanQueryTranslator = require('../lib/bq-translator').BooleanQueryTranslator;
+
+function testBooleanQueryTranslator(expected, query) {
+  test('translating query ' + query + ' -> ' + expected, function() {
+    var translator = new BooleanQueryTranslator();
+    assert.equal(expected, translator.translateTinia(query));
+  });
+}
+
+suite('BoolanQueryTranslator', function() {
+  testBooleanQueryTranslator('type:"ModelName"', "type:'ModelName'");
+  testBooleanQueryTranslator('query query type:"ModelName"', "(and query query type:'ModelName')");
+  testBooleanQueryTranslator('"query query" type:"ModelName"', "(and 'query query' type:'ModelName')");
+})
