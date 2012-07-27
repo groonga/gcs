04cb9d740acf53385c7bfb3da1463fc2c27fe49e
Kouhei Sutou <kou@clear-code.com>
2012-07-27 11:22:30 +0900

    test: add test target to test name

--- test/bq-translator.test.js
+++ test/bq-translator.test.js
@@ -2,18 +2,18 @@ var assert = require('chai').assert;
 
 var BooleanQueryTranslator = require('../lib/bq-translator').BooleanQueryTranslator;
 
-function testBooleanQueryTranslator(expected, query) {
-  test('translating query ' + query + ' -> ' + expected, function() {
+function testTranslate(expected, query) {
+  test('translate: <' + query + '> -> <' + expected + '>', function() {
     var translator = new BooleanQueryTranslator();
     assert.equal(expected, translator.translateTinia(query));
   });
 }
 
 suite('BoolanQueryTranslator', function() {
-  testBooleanQueryTranslator('type:"ModelName"',
-			     "type:'ModelName'");
-  testBooleanQueryTranslator('query query type:"ModelName"',
-			     "(and query query type:'ModelName')");
-  testBooleanQueryTranslator('"query query" type:"ModelName"',
-			     "(and 'query query' type:'ModelName')");
+  testTranslate('type:"ModelName"',
+                "type:'ModelName'");
+  testTranslate('query query type:"ModelName"',
+                "(and query query type:'ModelName')");
+  testTranslate('"query query" type:"ModelName"',
+                "(and 'query query' type:'ModelName')");
 })
